01e7c610994c4b48a8154e7efa031b5edae19d0b
burbokop <burbokop@gmail.com>
2020-12-30 04:09:23 +0200

    finish

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,22 @@
-#include "mandelbrot.h"
-
-#include <iostream>
-
 #include <src/gameapplication.h>
 #include <src/sdleventhandler.h>
 #include <src/sdlgraphicsprovider.h>
+#include <src/math/math.h>
+#include <src/graphics/imageview.h>
 
-using namespace std;
+const size_t width = 600;
+const size_t height = 600;
+const size_t depth = 32;
+const e172::Color colorMask = e172::argb(0xff, 0xff, 0x88, 0);
 
 int main(int argc, char **argv) {
     e172::GameApplication app(argc, argv);
-    SDLGraphicsProvider gp(app.arguments(), "mandelbrot", 600, 600);
+    SDLGraphicsProvider graphicsProvider(app.arguments(), "Mandelbrot", width, height);
+    SDLEventHandler eventHandler;
 
-    gp.loadFont("", "/usr/share/fonts/truetype/freefont/FreeMono.ttf");
-
-    app.setGraphicsProvider(&gp);
-    SDLEventHandler eh;
-    app.setEventHandler(&eh);
-
-    Mandelbrot mandelbrot;
-    app.addEntity(&mandelbrot);
-
+    e172::ImageView mandelbrotView = graphicsProvider.createImage(width, height, e172::Math::mandelbrot<uint32_t>(depth, colorMask));
+    app.setGraphicsProvider(&graphicsProvider);
+    app.setEventHandler(&eventHandler);
+    app.addEntity(&mandelbrotView);
     return app.exec();
 }
