7e493ec7074de979fc470ad787db66fbd0919bd5
burbokop <burbokop@gmail.com>
2020-12-30 14:32:46 +0200

    saving added

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@
 const size_t width = 600;
 const size_t height = 600;
 
+bool generateMandelbrotImage(e172::AbstractGraphicsProvider *graphicsProvider, const std::string &path, size_t N, size_t depth, e172::Color mask) {
+    return graphicsProvider->createImage(N, N, e172::Math::mandelbrot<uint32_t>(depth, mask)).save(path);
+}
+
 int main(int argc, char **argv) {
     size_t depth = 32;
     e172::Color colorMask = 0xff0000ff;
@@ -21,8 +25,14 @@ int main(int argc, char **argv) {
     SDLEventHandler eventHandler;
 
     e172::ImageView mandelbrotView = graphicsProvider.createImage(width, height, e172::Math::mandelbrot<uint32_t>(depth, colorMask));
+
     app.setGraphicsProvider(&graphicsProvider);
     app.setEventHandler(&eventHandler);
     app.addEntity(&mandelbrotView);
+
+    generateMandelbrotImage(&graphicsProvider, "./image1024.png", 1024, 32, colorMask);
+    generateMandelbrotImage(&graphicsProvider, "./image2048.png", 2048, 32, colorMask);
+    generateMandelbrotImage(&graphicsProvider, "./image4096.png", 4096, 32, colorMask);
+
     return app.exec();
 }
