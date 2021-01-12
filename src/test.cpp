5810fde6e690442ecc78603676a7817c7fadcab3
burbokop <burbokop@gmail.com>
2021-01-12 22:26:53 +0200

    gogadoda

--- src/test.cpp
+++ src/test.cpp
@@ -42,22 +42,16 @@ void resolution_test(const std::string &cache_path, std::ostream &out, size_t te
     constexpr size_t multiplier = 8;
 
     const auto file = cache_path + std::to_string(test_count) + ".json";
-    std::vector<double> x;
-    std::vector<double> y;
+
+    plt::sequence_pair xy;
     if(std::filesystem::exists(file)) {
         std::ifstream stream(file);
         std::string str((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
-        const auto data = e172::Variant::fromJson(str);
-
+        xy = e172::convert_to<plt::sequence_pair>(e172::Variant::fromJson(str));
         std::cout << "loaded from cache\n";
-
-        auto map = data.toMap();
-
-        x = convert_to<decltype (x)>(map["first"].toList());
-        y = convert_to<decltype (y)>(map["second"].toList());
     } else {
-        x = e172::Testing::make_set<double>(2, test_count, [](size_t x){ return x * multiplier; });
-        y = e172::Testing::compare_test<size_t>(x.begin(), x.end(), [](size_t N) {
+        xy.first = e172::Testing::make_set<double>(2, test_count, [](size_t x){ return x * multiplier; });
+        xy.second = e172::Testing::compare_test<size_t>(xy.first.begin(), xy.first.end(), [](size_t N) {
             uint32_t a[N * N];
             e172::Math::fractal<uint32_t>(depth, mask, e172::Math::sqr<e172::Complex>, false)(N, N, a);
         }, [](size_t N) {
@@ -67,27 +61,15 @@ void resolution_test(const std::string &cache_path, std::ostream &out, size_t te
         std::fstream() << std::string();
 
         std::ofstream(file, std::ios::out)
-                << e172::Variant(e172::toVariantMap(std::pair { e172::toVariantList(x), e172::toVariantList(y) })).toJson();
+                << e172::convert_to<e172::Variant>(xy).toJson();
     }
 
-    const auto integrated_xy = e172::intergate<1>(e172::discretize<0, 1>(std::pair { x, y }, multiplier));
-    const auto differentiated_y = e172::differentiate_vec({ x, y });
-    const auto differentiated_smoosed_xy = e172::intergate<1>(e172::discretize<0, 1>(std::pair { x, differentiated_y }, multiplier));
+    const auto integrated_xy = e172::intergate<1>(e172::discretize<0, 1>(std::pair { xy.first, xy.second }, multiplier));
+    const auto differentiated_y = e172::differentiate_vec({ xy.first, xy.second });
+    const auto differentiated_smoosed_xy = e172::intergate<1>(e172::discretize<0, 1>(std::pair { xy.first, differentiated_y }, multiplier));
 
-    const auto r0 = plt::make_plot(x, y);
-    const auto r1 = plt::make_plot(integrated_xy);
-    const auto r2 = plt::make_plot(x, differentiated_y);
-    const auto r3 = plt::make_plot(differentiated_smoosed_xy);
-    const auto mr = plt::make_plots(integrated_xy.first, { integrated_xy.second, differentiated_smoosed_xy.second });
-
-    std::system(("gnuplot " + r0.first->file_path()).c_str());
-    std::system(("gnuplot " + r1.first->file_path()).c_str());
-    std::system(("gnuplot " + r2.first->file_path()).c_str());
-    std::system(("gnuplot " + r3.first->file_path()).c_str());
-
-
+    const auto mr = plt::make_plots(integrated_xy.first, { integrated_xy.second, differentiated_smoosed_xy.second }, { 1 });
     std::system(("gnuplot " + mr.first->file_path()).c_str());
-
 }
 
 plt::plot plt::make_plot(const std::vector<double> &x, const std::vector<double> &y) {
@@ -108,7 +90,7 @@ plt::plot plt::make_plot(const std::vector<double> &x, const std::vector<double>
 }
 
 
-plt::plot plt::make_plots(const plt::sequence &x, const plt::sequence_vector &values) {
+plt::plot plt::make_plots(const plt::sequence &x, const plt::sequence_vector &values, const std::vector<double> &horisontalLines, int pause) {
     const auto csv_file = std::make_shared<tmp_file>("csv");
     const auto plot_file = std::make_shared<tmp_file>("plg");
 
@@ -126,7 +108,26 @@ plt::plot plt::make_plots(const plt::sequence &x, const plt::sequence_vector &va
         stream << "\n";
     }
 
-    std::string plot = "plot '" + csv_file->file_path() + "' title 'main' with lines, 1 with lines lt 1 title 'y = 1'\npause -1\n";
+    const auto cfp = "'" + csv_file->file_path() + "'";
+    std::string plot = "plot ";
+    for(size_t i = 0; i < horisontalLines.size(); ++i) {
+        const auto v = std::to_string(horisontalLines[i]);
+        plot += v + " with lines lt 1 title 'y = " + v + "'";
+        if(i < values.size() + horisontalLines.size() - 1) {
+            plot += ", ";
+        }
+    }
+    for(size_t i = 0; i < values.size(); ++i) {
+        plot += cfp + " using " + std::to_string(i + 2) + " title 'chart" + std::to_string(i) + "' with lines";
+        if(i < values.size() - 1) {
+            plot += ", ";
+        }
+    }
+    plot += "\n";
+    if(pause != 0) {
+        plot += "pause " + std::to_string(pause) + "\n";
+    }
+    std::cout << "plot text:\n" << plot << "\n";
     const auto csv = stream.str();
 
     plot_file->w(plot.c_str(), plot.size());
--- src/test.h
+++ src/test.h
@@ -10,25 +10,6 @@ void depth_test(std::ostream& out);
 void resolution_test(const std::string &cache_path, std::ostream& out, size_t test_count = 1024);
 
 
-template <typename T>
-T convert_to(const e172::VariantList &list) {
-    if constexpr(e172::sfinae::is_specialization<T, std::vector>::value) {
-        typedef typename T::value_type val_t;
-        if constexpr(std::is_integral<val_t>::value || std::is_same<val_t, double>::value || std::is_same<val_t, float>::value) {
-            T result;
-            result.reserve(list.size());
-            for(const auto& a : list) {
-                result.push_back(a.toNumber<val_t>());
-            }
-            return result;
-        } else {
-            return {};
-        }
-    } else {
-        return {};
-    }
-}
-
 namespace plt {
 
 typedef std::vector<double> sequence;
@@ -53,7 +34,7 @@ inline plot make_plot(const sequence_pair &xy) {
     return make_plot(xy.first, xy.second);
 }
 
-plot make_plots(const sequence &x, const sequence_vector &values);
+plot make_plots(const sequence &x, const sequence_vector &values, const std::vector<double> &horisontalLines = std::vector<double>(), int pause = -1);
 
 }
 
